fe20b47a8016d60b27139240d71e9f678ca192f6
Mike Zamayias <mzamagias@gmail.com>
2021-03-15 14:39:25 +0200

    made class attributes final and changed the return value of method toString

--- lab/assignments/assignment-1/code/src/com/company/Disk.java
+++ lab/assignments/assignment-1/code/src/com/company/Disk.java
@@ -1,20 +1,19 @@
 public class Disk extends Hardware {
-    private String brand; // disk brand
-    private String type; // disk type (hdd or ssd)
-    private double capacity; // disk capacity in tb
+    private final String brand; // disk brand
+    private final String type; // disk type (hdd or ssd)
+    private final double capacity; // disk capacity in tb
 
     public Disk(String brand, String type, double capacity) {
         super("Disk");
         this.brand = brand;
         this.type = type;
         this.capacity = capacity;
-        super.counter++;
     }
 
     @Override
     public String toString() {
         // "Disk 0: HW Disk brand: WD 3.0 TB HDD"
-        return "Disk %s: %s brand: %s %s TB %s".formatted(super.getCounter(), super.toString(), brand, capacity, this.type);
+        return "%s brand: %s %s TB %s".formatted(super.toString(), brand, capacity, this.type);
     }
 
     public double getCapacity() {
