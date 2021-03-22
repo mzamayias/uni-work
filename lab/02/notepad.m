bcd29db17117b330249d07c8e25cd2a207d6d697
Mike Zamayias <mzamagias@gmail.com>
2021-03-22 23:17:22 +0200

    added two examples from the lecture

--- lab/02/notepad.m
+++ lab/02/notepad.m
@@ -10,7 +10,26 @@ term2 = 0.75 * delta_function(-2, x);
 term3 = delta_function(-3, x);
 y = term0 + term1 + term2 + term3;
 f1 = plot_figure(1, x, y, x, [0:0.1:1.5]);
-waitfor(f1);
 % <<<
 
+% >>> Lab_04_Students.pdf - Page 25 of 32
+% y(n) = -0.9 * y(n-1) + x(n)
+% h(n) = -0.9 * h(n-1) + d(n)
+n = 0:100;
+a = [1, 0.9]; % h coefficients
+b = [1, 0]; % d coefficients
+h = filter(b, a, delta_function(0, n));
+f2 = plot_figure(2, n, h, [0:10:100], [-1:0.1:1]);
+% <<<
 
+% >>> Lab_04_Students.pdf - Page 23 of 32
+% y(n) = -0.9 * y(n-1) + x(n)
+% h(n) = -0.9 * h(n-1) + d(n)
+d = @(n) n == 0;
+h(1) = 1;
+for n = 2:101
+    h(n) = -0.9 * h(n - 1) + d(n);
+endfor
+f3 = plot_figure(3, 0:100, h, [0:10:100], [-1:0.1:1]);
+waitfor(f3);
+% <<<
