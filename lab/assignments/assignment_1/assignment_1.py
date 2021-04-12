6c4792eff2fa8df9729957af29ccea0137d42390
Mike Zamayias <mzamagias@gmail.com>
2021-04-12 08:58:52 +0300

    minor changes to python script

--- lab/assignments/assignment_1/assignment_1.py
+++ lab/assignments/assignment_1/assignment_1.py
@@ -1,79 +1,41 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Imports
-
-# In[ ]:
-
-
 import pandas as pd
 import seaborn as sns
+from scipy import stats
 
-
-# # Data pre-processing
-
-# In[ ]:
-
-
-genes = pd.read_csv('http://139.91.190.186/tei/bioinformatics/assignment.txt', sep='\t').iloc[:, 1:]
-
-
-# In[ ]:
-
-
+#   Read gene data
+genes = pd.read_csv(
+    "http://139.91.190.186/tei/bioinformatics/assignment.txt", sep="\t").iloc[:, 1:]
+#   Plot first 40 genes
 sns.heatmap(genes.iloc[:40])
 
+# Calculate cancer class mean
+genes["Cancer.Mean"] = genes.filter(like="Cancer").mean(axis=1)
+# Calculate normal class mean
+genes["Normal.Mean"] = genes.filter(like="Normal").mean(axis=1)
+# Calculate class mean delta
+genes["Means.Delta"] = genes["Cancer.Mean"] - genes["Normal.Mean"]
 
-# # Data analysis
+# Sort genes by 
+means_delta_sorted_genes = genes.sort_values(by="Means.Delta")
 
-# ## Gene expression analysis using means
-
-# In[ ]:
-
-
-genes['Cancer.Mean'] = genes.filter(like='Cancer').mean(axis=1)
-
-
-# In[ ]:
-
-
-genes['Normal.Mean'] = genes.filter(like='Normal').mean(axis=1)
-
-
-# In[ ]:
-
-
-genes['Means.Delta'] = genes['Cancer.Mean'] - genes['Normal.Mean']
-
-
-# In[ ]:
-
-
-means_delta_sorted_genes = genes.sort_values(by='Means.Delta')
-
-
-# In[ ]:
-
-
-sliced_means_delta_sorted_genes = means_delta_sorted_genes.head(20).append(means_delta_sorted_genes.tail(20))
-
-
-# In[ ]:
-
+sliced_means_delta_sorted_genes = pd.concat(
+    [means_delta_sorted_genes.head(20), means_delta_sorted_genes.tail(20)],
+    axis=0
+)
 
 sns.heatmap(sliced_means_delta_sorted_genes.iloc[:, :-3])
 
+genes["p-value"] = [
+    stats.ttest_ind(
+        genes.iloc[gene_index, :8], genes.iloc[gene_index, 8:15]
+    )[-1]
+    for gene_index in range(genes.shape[0])
+]
 
-# ## Gene expression analysis using p-value
+means_delta_sorted_genes = genes[genes['p-value']
+                                 < 0.05].sort_values(by="Means.Delta")
 
-# ## Gene expression analysis using Bonferroni corrected p-value
+sliced_pvalue_means_delta_sorted_genes = pd.concat(
+    [means_delta_sorted_genes.head(20), means_delta_sorted_genes.tail(20)], axis=0)
 
-# ## Gene expression analysis using q-value
-
-# # Data Validation
-
-# In[ ]:
-
-
-
-
+sns.heatmap(sliced_pvalue_means_delta_sorted_genes.iloc[:, :-4])
