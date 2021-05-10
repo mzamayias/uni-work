e5dab8b5cd0e1efae8a12f31afe3822d430f99c6
Mike Zamayias <mzamagias@gmail.com>
2021-05-10 15:02:40 +0300

    worked on the scratch book

--- lab/assignments/assignment_2/scratch_book.py
+++ lab/assignments/assignment_2/scratch_book.py
@@ -1,9 +1,19 @@
 import pandas as pd
+
+#   >>> Data pre-processing
+
+# training_data = 'http://139.91.190.186/tei/bioinformatics/LungTrain.txt'
+# test_data = 'http://139.91.190.186/tei/bioinformatics/LungTest.txt'
+# kegg_data = 'http://139.91.190.186/tei/bioinformatics/c2.cp.kegg.v7.4.entrez.gmt'
+
+datasets = ['KEGG_DATA.txt', 'LungTest.txt', 'LungTrain.txt']
+# training_data, test_data, kegg_data = datasets[::]
+
 training_data, test_data, kegg_data = [
-    'http://139.91.190.186/tei/bioinformatics/LungTrain.txt',
-    'http://139.91.190.186/tei/bioinformatics/LungTest.txt',
-    'http://139.91.190.186/tei/bioinformatics/c2.cp.kegg.v7.4.entrez.gmt'
+    pd.read_csv(dataset, sep='\t').head(1)
+    for dataset in datasets
 ]
 
-for data in [training_data, test_data, kegg_data]:
-    print(type(data))
+
+
+#   <<< Data pre-processing
