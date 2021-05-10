3c99c0650f5a70c820a9e2d7f80d4d3d4017bf9f
Mike Zamayias <mzamagias@gmail.com>
2021-05-10 17:54:28 +0300

    updated code

--- lab/assignments/assignment_2/scratch_book.py
+++ lab/assignments/assignment_2/scratch_book.py
@@ -10,10 +10,11 @@ datasets = ['KEGG_DATA.txt', 'LungTest.txt', 'LungTrain.txt']
 # training_data, test_data, kegg_data = datasets[::]
 
 training_data, test_data, kegg_data = [
-    pd.read_csv(dataset, sep='\t').head(1)
+    pd.read_csv(dataset, sep='\t')
     for dataset in datasets
 ]
 
-
+print(training_data.head(5))
+print(training_data.T.head(5))
 
 #   <<< Data pre-processing
