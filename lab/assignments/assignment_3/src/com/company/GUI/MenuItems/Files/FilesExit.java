507a3eaf83b9b01dae288769f74ac424aa9d49e8
Mike Zamayias <mzamagias@gmail.com>
2021-05-26 00:18:30 +0300

    exit in menu can now exit the application.

--- lab/assignments/assignment_3/src/com/company/GUI/MenuItems/Files/FilesExit.java
+++ lab/assignments/assignment_3/src/com/company/GUI/MenuItems/Files/FilesExit.java
@@ -1,9 +1,10 @@
 package com.company.GUI.MenuItems.Files;
 
-import javax.swing.*;
+import javax.swing.JMenuItem;
 
-public class FilesExit extends JMenuItem {
+public class FilesExit extends JMenuItem{
     public FilesExit() {
         setText("Exit");
+        addActionListener(l -> System.exit(0));
     }
 }
