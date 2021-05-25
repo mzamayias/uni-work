74f6768e3dca81e1ecb938b18143f2f6422a56dc
Mike Zamayias <mzamagias@gmail.com>
2021-05-25 23:56:28 +0300

    updated main for the last code refactor.

--- lab/assignments/assignment_3/src/com/company/Main.java
+++ lab/assignments/assignment_3/src/com/company/Main.java
@@ -1,10 +1,11 @@
 package com.company;
 
-import static com.company.DatabaseConnector.databaseConnector;
+import com.company.GUI.Frame;
 
 public class Main {
 
     public static void main(String[] args) {
-        databaseConnector();
+//        databaseConnector();
+        new Frame();
     }
 }
