c7ed609abc3487e71fd901df79eec09f52e0748d
Mike Zamayias <mzamagias@gmail.com>
2021-05-26 04:35:28 +0300

    added  dialog panel in files > customers.

--- lab/assignments/assignment_3/src/com/company/GUI/MenuItems/Files/FilesCustomers.java
+++ lab/assignments/assignment_3/src/com/company/GUI/MenuItems/Files/FilesCustomers.java
@@ -1,9 +1,12 @@
 package com.company.GUI.MenuItems.Files;
 
+import com.company.GUI.Dialogs.DialogCustomers;
+
 import javax.swing.*;
 
 public class FilesCustomers extends JMenuItem {
     public FilesCustomers() {
         setText("Customers");
+        addActionListener(l -> new DialogCustomers());
     }
 }
