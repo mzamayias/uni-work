b93a335be92ed2780d7d472e6d73d6ea889c6a35
Mike Zamayias <mzamagias@gmail.com>
2021-05-26 05:26:24 +0300

    about dialog is done for now.

--- lab/assignments/assignment_3/src/com/company/GUI/Dialogs/Help/HelpDialogAbout.java
+++ lab/assignments/assignment_3/src/com/company/GUI/Dialogs/Help/HelpDialogAbout.java
@@ -7,12 +7,36 @@ import java.awt.*;
 
 public class HelpDialogAbout extends BaseDialog {
     public HelpDialogAbout() {
-        setTitle("About");
-
         JPanel jPanel = new JPanel();
-        jPanel.setLayout(new FlowLayout());
-        add(jPanel);
+        JLabel[] labelsAuthor = {
+                new JLabel("Application Author:"),
+                new JLabel("Mike Zamayias")
+        };
+        JLabel[] labelsVersion = {
+                new JLabel("Application Version:"),
+                new JLabel("Under Development")
+        };
+        JLabel[] labelsReach = {
+                new JLabel("GitHub:"),
+                new JLabel("@mzamayias")
+        };
 
+        jPanel.setLayout(new GridLayout(3,2));
+        for (JLabel jLabel : labelsAuthor) {
+            jPanel.add(jLabel);
+        }
+        for (JLabel jLabel : labelsVersion) {
+            jPanel.add(jLabel);
+        }
+        for (JLabel jLabel : labelsReach) {
+            jPanel.add(jLabel);
+        }
+
+        setTitle("About");
+        add(jPanel);
+        pack();
+        setSize(300,150);
+        setResizable(false);
         setVisible(true);
     }
 }
