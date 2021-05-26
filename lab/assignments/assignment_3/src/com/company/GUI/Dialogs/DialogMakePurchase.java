3ce78451be0ad67da73d62b0f39a0c34e7bf43dc
Mike Zamayias <mzamagias@gmail.com>
2021-05-26 04:36:24 +0300

    applied oop.

--- lab/assignments/assignment_3/src/com/company/GUI/Dialogs/DialogMakePurchase.java
+++ lab/assignments/assignment_3/src/com/company/GUI/Dialogs/DialogMakePurchase.java
@@ -3,16 +3,12 @@ package com.company.GUI.Dialogs;
 import javax.swing.*;
 import java.awt.*;
 
-public class DialogMakePurchase extends JDialog {
+public class DialogMakePurchase extends BaseDialog {
     public DialogMakePurchase() {
         setTitle("Make Purchase");
-        setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
-        setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
-        setVisible(true);
 
         JPanel jPanel = new JPanel();
         jPanel.setLayout(new FlowLayout());
         add(jPanel);
-        pack();
     }
 }
