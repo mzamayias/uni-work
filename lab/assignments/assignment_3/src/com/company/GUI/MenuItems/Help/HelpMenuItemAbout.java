20dfa18c50a634598d1f365f6370f6d278f88f4d
Mike Zamayias <mzamagias@gmail.com>
2021-05-26 04:56:44 +0300

    dialog now opens, in help > about

--- lab/assignments/assignment_3/src/com/company/GUI/MenuItems/Help/HelpMenuItemAbout.java
+++ lab/assignments/assignment_3/src/com/company/GUI/MenuItems/Help/HelpMenuItemAbout.java
@@ -1,9 +1,12 @@
 package com.company.GUI.MenuItems.Help;
 
+import com.company.GUI.Dialogs.Help.HelpDialogAbout;
+
 import javax.swing.JMenuItem;
 
 public class HelpMenuItemAbout extends JMenuItem {
     public HelpMenuItemAbout() {
         setText("About");
+        addActionListener(l -> new HelpDialogAbout());
     }
 }
