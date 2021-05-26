9da63d368df7e9d52d11072ecb692d01e8c98578
Mike Zamayias <mzamagias@gmail.com>
2021-05-26 04:36:11 +0300

    decoupled code, made it more oop-like.

---
+++ lab/assignments/assignment_3/src/com/company/GUI/Dialogs/BaseDialog.java
@@ -0,0 +1,13 @@
+package com.company.GUI.Dialogs;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BaseDialog extends JDialog {
+    public BaseDialog() {
+        setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
+        setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
+        setVisible(true);
+        pack();
+    }
+}
