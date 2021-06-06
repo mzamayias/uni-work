6873ec09aebd8965b8b884fde442a5e7959e0ca6
Mike Zamayias <mzamagias@gmail.com>
2021-06-06 13:51:05 +0300

    database refactoring related

--- lab/assignments/assignment_3/src/com/company/GUI/MenuBar.java
+++ lab/assignments/assignment_3/src/com/company/GUI/MenuBar.java
@@ -2,7 +2,7 @@ package com.company.GUI;
 
 import com.company.GUI.Menus.FilesMenu;
 import com.company.GUI.Menus.HelpMenu;
-import com.company.GUI.Menus.PurchasesMenu;
+import com.company.GUI.Menus.OrdersMenu;
 import com.company.GUI.Menus.ReportsMenu;
 
 import javax.swing.JMenuBar;
@@ -10,7 +10,7 @@ import javax.swing.JMenuBar;
 public class MenuBar extends JMenuBar {
     public MenuBar() {
         add(new FilesMenu());
-        add(new PurchasesMenu());
+        add(new OrdersMenu());
         add(new ReportsMenu());
         add(new HelpMenu());
     }
