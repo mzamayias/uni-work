92de28426165e9241b29f3efcfd7546f2524ca60
Mike Zamayias <mzamagias@gmail.com>
2021-06-12 11:08:04 +0300

    removed a unused line

--- lab/assignments/assignment_3/src/com/company/Main.java
+++ lab/assignments/assignment_3/src/com/company/Main.java
@@ -1,13 +1,11 @@
 package com.company;
 
-import static com.company.Controller.DatabaseConnector.databaseConnector;
 
 import com.company.View.Frame;
 
 public class Main {
 
     public static void main(String[] args) {
-//        databaseConnector();
         new Frame();
     }
 }
