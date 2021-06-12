2f936838124a7ec6e34c2269919e507641dbebb6
Mike Zamayias <mzamagias@gmail.com>
2021-06-12 14:42:30 +0300

    it's showing something

--- lab/assignments/assignment_3/src/com/company/View/Dialogs/Orders/OrdersDialogPlaceOrder.java
+++ lab/assignments/assignment_3/src/com/company/View/Dialogs/Orders/OrdersDialogPlaceOrder.java
@@ -4,6 +4,8 @@ import com.company.View.Dialogs.BaseDialog;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class OrdersDialogPlaceOrder extends BaseDialog {
     public OrdersDialogPlaceOrder() {
@@ -16,9 +18,27 @@ public class OrdersDialogPlaceOrder extends BaseDialog {
 
         panel.setLayout(new FlowLayout());
 
-        add(panel);
-
         setTitle("Place Order");
         setVisible(true);
+
+        JTable table = new JTable();
+
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent evt) {
+                int row = table.rowAtPoint(evt.getPoint());
+                int col = table.columnAtPoint(evt.getPoint());
+                if (row >= 0 && col >= 0) {
+                    System.out.println("Selected: Row: " + row + " Column: " + col);
+                    Object objectAtClick = table.getModel().getValueAt(row, col);
+                    System.out.println("StudentID is: " + table.getModel().getValueAt(row, 0) + " - Object clicked is: " + objectAtClick);
+                }
+            }
+        });
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        panel.add(scrollPane);
+        add(panel);
+        pack();
     }
 }
