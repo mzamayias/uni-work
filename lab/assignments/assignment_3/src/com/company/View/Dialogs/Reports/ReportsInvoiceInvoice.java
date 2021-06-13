439c139471dda8a3c054e458aa7274896fbc1fca
Mike Zamayias <mzamagias@gmail.com>
2021-06-13 18:13:59 +0300

    there's a combobox here now

--- lab/assignments/assignment_3/src/com/company/View/Dialogs/Reports/ReportsInvoiceInvoice.java
+++ lab/assignments/assignment_3/src/com/company/View/Dialogs/Reports/ReportsInvoiceInvoice.java
@@ -3,12 +3,10 @@ package com.company.View.Dialogs.Reports;
 import com.company.View.Dialogs.BaseDialog;
 import com.company.View.Dialogs.UniversalComboBox;
 
+import javax.swing.*;
 import java.awt.*;
-import java.sql.ResultSet;
 
 public class ReportsInvoiceInvoice extends BaseDialog {
-    private ResultSet resultSet = null;
-
     public ReportsInvoiceInvoice() {
         super(new Dimension(309, 99));
         databaseConnection();
@@ -17,15 +15,28 @@ public class ReportsInvoiceInvoice extends BaseDialog {
 
     private void improveBaseDialog() {
         // initialize and declare objects
+        JPanel panel = new JPanel(new GridLayout(2, 1));
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        JButton button = new JButton("Print");
         UniversalComboBox informationCustomers = new UniversalComboBox(
                 "Invoice for customer:",
                 new ReportsComboModelCustomers(connection)
         );
 
-        // add universal combobox to base dialog
-        add(informationCustomers, BorderLayout.EAST);
+        // set button preferences
+        buttonPanel.add(button);
+
+        // add universal combobox to panel
+        panel.add(informationCustomers);
+        panel.add(buttonPanel);
+
+        // add panel to dialog
+        add(panel);
 
         // base dialog preferences
         setTitle("Print Invoice");
+
+        // set button actions
+        button.addActionListener(l -> System.out.println(informationCustomers.getSelectedItem()));
     }
 }
