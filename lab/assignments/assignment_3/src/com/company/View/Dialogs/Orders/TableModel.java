f59ed3f01a786f003d388add16a0e4458d36fbf3
Mike Zamayias <mzamagias@gmail.com>
2021-06-13 18:14:07 +0300

    works better

--- lab/assignments/assignment_3/src/com/company/View/Dialogs/Orders/TableModel.java
+++ lab/assignments/assignment_3/src/com/company/View/Dialogs/Orders/TableModel.java
@@ -7,10 +7,15 @@ public class TableModel extends AbstractTableModel {
     protected static Connection connection;
     protected static Statement statement;
     protected static ResultSet resultSet;
-    protected static String query = "SELECT * FROM inventory_logistics.orders" +
-            " LEFT JOIN inventory_logistics.customers ON customers.customer_id=orders.customer_id" +
-            " LEFT JOIN inventory_logistics.inventory ON inventory.inventory_id=orders.inventory_id";
-    protected static String[] columnNames = {"order_id", "customer_id", "inventory_id", "quantity", "price"};
+    protected static String query = """
+            SELECT *
+            FROM inventory_logistics.orders
+                     LEFT JOIN inventory_logistics.customers ON customers.customer_id = orders.customer_id
+                     LEFT JOIN inventory_logistics.inventory ON inventory.inventory_id = orders.inventory_id
+                     """;
+    protected static String[] columnNames = {
+            "orders.order_id", "customers.last_name", "inventory.category", "inventory.description", "inventory.price"
+    };
 
     public TableModel() {
         String url = "jdbc:mysql://localhost:3306";
@@ -51,11 +56,11 @@ public class TableModel extends AbstractTableModel {
         try {
             resultSet.absolute(rowIndex + 1);
             switch (columnIndex) {
-                case 0 -> returnValue = resultSet.getInt(columnNames[0]);
-                case 1 -> returnValue = resultSet.getInt(columnNames[1]);
+                case 0 -> returnValue = resultSet.getString(columnNames[0]);
+                case 1 -> returnValue = resultSet.getString(columnNames[1]);
                 case 2 -> returnValue = resultSet.getString(columnNames[2]);
                 case 3 -> returnValue = resultSet.getString(columnNames[3]);
-                case 4 -> returnValue = resultSet.getInt(columnNames[4]);
+                case 4 -> returnValue = resultSet.getString(columnNames[4]);
             }
         } catch (SQLException throwable) {
             throwable.printStackTrace();
