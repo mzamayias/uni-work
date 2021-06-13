6f7edada998eda7f9b21ad720acac0ff5f087ea4
Mike Zamayias <mzamagias@gmail.com>
2021-06-13 06:49:40 +0300

    based on InformationPanel

--- lab/assignments/assignment_3/src/com/company/View/Dialogs/InformationComboBox.java
+++ lab/assignments/assignment_3/src/com/company/View/Dialogs/InformationComboBox.java
@@ -1,4 +1,28 @@
 package com.company.View.Dialogs;
 
-public class InformationComboBox {
+import javax.swing.*;
+import java.awt.*;
+
+public class InformationComboBox extends JPanel {
+    private final JLabel label = new JLabel();
+    private final JComboBox<String> comboBox = new JComboBox<String>();
+
+    public InformationComboBox(String elementName) {
+        prepareElementPanel(elementName);
+    }
+
+    private void prepareElementPanel(String elementName) {
+        setLayout(new GridLayout(1, 2));
+
+        label.setText(elementName);
+        label.setHorizontalAlignment(SwingConstants.RIGHT);
+        add(label);
+
+        comboBox.setPreferredSize(new Dimension(270, 12));
+        add(comboBox);
+    }
+
+    public void setEditable(boolean editable) {
+        comboBox.setEditable(editable);
+    }
 }
