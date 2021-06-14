b4321f0231aaa0113a3c623a11a26b0828335bc9
Mike Zamayias <mzamagias@gmail.com>
2021-06-14 07:23:58 +0300

    refactor

--- lab/assignments/assignment_3/src/com/company/View/Dialogs/Help/HelpDialogAbout.java
+++ lab/assignments/assignment_3/src/com/company/View/Dialogs/Help/HelpDialogAbout.java
@@ -8,6 +8,11 @@ import java.awt.*;
 
 public class HelpDialogAbout extends BaseDialog {
     public HelpDialogAbout() {
+        super(new Dimension(300, 90));
+        improveBaseDialog();
+    }
+
+    private void improveBaseDialog() {
         JPanel panel = new JPanel(new GridLayout(3, 1));
         InformationTextField informationAppAuthor = new InformationTextField(
                 "Application Author:",
@@ -26,6 +31,7 @@ public class HelpDialogAbout extends BaseDialog {
         };
 
         for (InformationTextField informationTextField : informationTextFields) {
+            informationTextField.setEditable(false);
             panel.add(informationTextField);
         }
 
