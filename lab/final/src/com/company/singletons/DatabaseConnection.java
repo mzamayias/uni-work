0474eb21a072a0f931e98e2a62b2b19d91c92f86
Mike Zamayias <mzamagias@gmail.com>
2021-06-16 15:40:58 +0300

    database connection as singleton

---
+++ lab/final/src/com/company/singletons/DatabaseConnection.java
@@ -0,0 +1,27 @@
+package com.company.singletons;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    static final String url = "jdbc:mysql://localhost:3306/sakila?useSSL=false";
+    static final String user = "root";
+    static final String password = "secret";
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Hello!");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        return connection;
+    }
+}
